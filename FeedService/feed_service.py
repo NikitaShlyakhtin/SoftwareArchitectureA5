c20f9293e16e2c48b19b44bdd05af42985981c68
Karim <k.nasybullin@innopolis.university>
2024-10-09 19:53:49 +0300

    adding feed service

---
+++ FeedService/feed_service.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import UUID
+from uuid6 import uuid7
+
+app = Flask('feed_service')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@db:5432/a5'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Message(db.Model):
+    tablename = 'messages'
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid7())
+    content = db.Column(db.String, nullable=False)
+    timestamp = db.Column(db.TIMESTAMP, nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/feed', methods=['GET'])
+def get_feed():
+    messages = Message.query.order_by(Message.timestamp.desc()).limit(10).all()
+    messages_list = [{'id': str(message.id), 'content': message.content, 'timestamp': message.timestamp.isoformat()} for message in messages]
+    return jsonify(messages_list)
+
+app.run(debug=True)
